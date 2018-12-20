18f9ce0f480119826ac375380f76eb0482eb75e5
ANCLO95 <andre.cloutier.4@ulaval.ca>
2018-12-20 16:12:22 -0500

    sss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome Comment cela est-il to Your Vue.js App'
     }
   }
 }
