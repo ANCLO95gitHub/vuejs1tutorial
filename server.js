8ad1e777db35b64e0f30199b9e6839a4252928aa
ANCLO95 <andre.cloutier.4@ulaval.ca>
2018-12-23 15:23:50 -0500

    server dyna

--- server.js
+++ server.js
@@ -14,15 +14,17 @@ app.use(express.static(__dirname + "/dist"));
 app.use(bodyParser());
 app.use(cors(corsOptions));
 
-app.get(/.*/, function( request, response) {
-  //console.log( 'serveur Heroku Default' );
+
+//app.get(/.*/, function( request, response) {
+/*  //console.log( 'serveur Heroku Default' );
   response.sendfile(__dirname + "/dist/index.html");
 });
+*/
 app.get('/', (req, res) => res.send('Labo vues1tutorial  en testing'));
 app.get('/ceci', function( request, response) {
   console.log( 'serveur Heroku get ceci' );
   //response.sendfile(__dirname + "/dist/index.html");
-  response.status(200).send('Welcome to vuejs1tutorial API.  Server Side...')
+  response.status(200).send('Welcome to vuejs1tutorial API.  Server Side... (dimanche le 23 DEC 2018.')
   //res.render('salutation.ejs', {username: salut}));
 });
 
